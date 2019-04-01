cf1cc532aa06fb5c5a31b8fe9d5315456f70d387
ataul <ataul.haque@gmail.com>
2019-04-01 12:19:39 +0600

    structure export

--- index.php
+++ index.php
@@ -10,6 +10,30 @@ $stm = $pdo->query("SHOW TABLES");
 $data = $stm->fetchAll();
 
 foreach($data as $d){
-	echo $d[0].'<br />';
+	$table = $d[0];
+	$sql = export_structure($table);
+	//echo $d[0].'<br />';
+	echo $sql.'<br />';
+}
+
+function export_structure($table){
+	global $pdo;
+	$stm = $pdo->query("DESCRIBE $table");
+	$data = $stm->fetchAll();
+	
+	$sql = "CREATE TABLE IF NOT EXISTS `$table` (";
+	$fields = array();
+	
+	foreach($data as $d){
+		$field="`$d[Field]` $d[Type] ";
+		if($d['Null']=='NO'){
+			$field.= " NOT NULL";	
+		}else{
+			$field.= " NULL";
+		}
+		$fields[]=$field;
+	}
+	$sql.=implode(',',$fields).');';
+	return $sql;
 }
 ?>
