3989a2457af9abf6d4d100613263666e3af2d82c
ataul <ataul.haque@gmail.com>
2019-04-01 13:10:33 +0600

    data export

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-$user = "root";
-$passwd = "";
-$dbname = "db";
+require_once('db_config.php');
 
 $dsn = "mysql:host=localhost;dbname=".$dbname;
 $pdo = new PDO($dsn, $user, $passwd);
@@ -13,17 +11,18 @@ foreach($data as $d){
 	$table = $d[0];
 	$sql = export_structure($table);
 	//echo $d[0].'<br />';
-	echo $sql.'<br />';
+	//echo $sql.'<br />';
+	
 }
+$sql = export_data($data[0][0],0,100);
+echo $sql;
 
 function export_structure($table){
 	global $pdo;
 	$stm = $pdo->query("DESCRIBE $table");
-	$data = $stm->fetchAll();
-	
+	$data = $stm->fetchAll();	
 	$sql = "CREATE TABLE IF NOT EXISTS `$table` (";
-	$fields = array();
-	
+	$fields = array();	
 	foreach($data as $d){
 		$field="`$d[Field]` $d[Type] ";
 		if($d['Null']=='NO'){
@@ -34,6 +33,22 @@ function export_structure($table){
 		$fields[]=$field;
 	}
 	$sql.=implode(',',$fields).');';
+	return $sql;
+}
+function export_data($table,$start,$limit){
+	global $pdo;
+	$stm = $pdo->query("SELECT * FROM $table LIMIT $start,$limit");
+	$data = $stm->fetchAll();
+	$sql = "INSERT INTO $table VALUES(";
+	echo '<pre>';
+	foreach($data as $d){
+		$s = "";
+		for($i=0;$i<sizeof($d)/2;$i++){
+			$s .= "'".$d[$i]."',";
+		}
+		$sql .= substr($s,0,strlen($s)-1)."),(";		
+	}
+	$sql = substr($sql,0,strlen($sql)-2).";";
 	return $sql;
 }
 ?>
